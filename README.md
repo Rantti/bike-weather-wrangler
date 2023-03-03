f3ba85d9f498d05c51ae4d27a5a621cb27a45813
Rantti <antti.o.eloranta@gmail.com>
2023-03-03 20:28:31 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,57 @@
+# Weather & Bike data wrangler
+
+Quick throwaway script for combining 3 sources of data into one datasheet
+- Weather data per hour
+- Bike rides from a given station ( or as many bike-stations as you want )
+
+You can edit the parser as you want, I just made it to fit certain needs.
+
+## Setup
+
+### Installation
+
+Start out by installing the packages with `npm ci`.
+
+### Input - adding datafiles
+
+#### Weather
+
+Expected weather data in CSV:
+
+Save this file into the root of project as `weather.csv`
+
+```
+Year,m,d,Time,Time zone,Precipitation amount (mm),Air temperature (degC),Wind speed (m/s)
+2021,4,23,4:00,UTC,5.4,0.7,1.2
+```
+
+**Pay attention to the time!** If you are who I think you are, you will have the time with 04:00 in your data for all timestamps between 00 - 09.
+
+#### Rides
+
+Expected ride data in CSV:
+Save this file into the root of project as `rides.csv`
+
+```
+Departure;Departure station id;Departure station name;Return station id;Return station name;Covered distance (m)
+1.4.2021 6:55;20;Kaisaniemenpuisto;60;Kaapelitehdas;3250
+```
+
+#### All rides
+
+Expected "all rides" data in CSV:
+Save this file into the root of project as `allrides.csv`
+
+```
+Timestamp;Rides
+1.4.2021 1:00;0
+```
+
+## Usage
+
+Use the tool with npm:
+
+```
+$ cd /path/to/project
+$ npm run generate
+```
---
+++ index.js
@@ -0,0 +1,118 @@
+const fs = require('fs');
+const { parse } = require('csv-parse');
+const { stringify } = require('csv-stringify');
+
+const FILE_NAME_RESULT = 'result.csv';
+const FILE_NAME_WEATHER_DATA = 'weather.csv';
+const FILE_NAME_BIKE_DATA = 'rides.csv';
+const FILE_NAME_ALL_BIKE_DATA = 'allrides.csv';
+
+const weatherRows = [];
+
+// For debug
+const failedBikeMatches = [];
+const failedTotalBikeMatches = [];
+
+/**
+ * Read weather-data and save rows to weatherRows-object.
+ */
+function importWeatherRows() {
+    fs.createReadStream(`./${FILE_NAME_WEATHER_DATA}`)
+        .pipe(parse({ delimiter: ',', from_line: 2 }))
+        .on('data', function (row) {
+            const [year, month, day, time, , precipitation, temperature, wind] = row;
+            const timestamp = `${day}.${month}.${year} ${time}`;
+            const rowObject = { timestamp, precipitation, temperature, wind, rides: 0, total_rides: 0 };
+            weatherRows.push(rowObject);
+        })
+        .on('end', function () {
+            console.log('finished');
+            addRides();
+        })
+        .on('error', function ({ message }) {
+            console.error('ERROR WHEN PARSING WEATHER DATA', message);
+        });
+}
+
+/**
+ * Compare timestamp of both given rows.
+ *
+ * @FIXME: format biketime to correct format in advance (e.g. 21:00 instead of 21:06)
+ * @param {*} bikeRow
+ * @param {*} weatherRow
+ * @param {boolean} debug - use this to compare data in weather vs ride -rows
+ * @returns
+ */
+function bikeRowMatches(bikeRow, weatherRow, debug = false) {
+    const { departure_time: bikeTime } = bikeRow;
+    const { timestamp: weatherTimeStamp } = weatherRow;
+
+    const bikeTimeStamp = bikeTime.slice(0, -2) + '00';
+
+    if (debug) {
+        console.log(`${bikeTime} vs ${weatherTimeStamp}`);
+    }
+
+    return bikeTimeStamp === weatherTimeStamp;
+}
+
+function addRides() {
+    console.log('Adding rides');
+    fs.createReadStream(`./${FILE_NAME_BIKE_DATA}`)
+        .pipe(parse({ delimiter: ';', from_line: 2 }))
+        .on('data', function (row) {
+            const [departure_time] = row;
+            const bikeRowObject = { departure_time };
+            const matchingWeatherTimeStampIdx = weatherRows
+                .findIndex(weatherRow => bikeRowMatches(bikeRowObject, weatherRow));
+            if (matchingWeatherTimeStampIdx !== -1) {
+                weatherRows[matchingWeatherTimeStampIdx].rides++;
+            } else {
+
+                failedBikeMatches.push(bikeRowObject);
+            }
+        })
+        .on('end', function () {
+            console.log(`Added rides, there were ${failedBikeMatches.length} not matched.`);
+            addTotalRides();
+        })
+        .on('error', function (error) {
+            console.error('ERROR WHEN PARSING RIDES', error.message);
+        });
+}
+
+function addTotalRides() {
+    console.log('Adding total rides.');
+    fs.createReadStream(`./${FILE_NAME_ALL_BIKE_DATA}`)
+        .pipe(parse({ delimiter: ';', from_line: 2 }))
+        .on('data', function (row) {
+            const [departure_time, totalRides] = row;
+            const totalRidesObject = { departure_time, totalRides };
+            const matchingWeatherTimeStampIdx = weatherRows.findIndex(weatherRow => bikeRowMatches(totalRidesObject, weatherRow));
+            if (matchingWeatherTimeStampIdx !== -1) {
+                weatherRows[matchingWeatherTimeStampIdx].total_rides = totalRides;
+            } else {
+                failedTotalBikeMatches.push(totalRidesObject);
+            }
+        })
+        .on('end', function () {
+            console.log(`Added total rides, there were ${failedTotalBikeMatches.length} not matched.`);
+            writeToFile();
+        })
+        .on('error', function (error) {
+            console.error('ERROR WHEN PARSING ALL RIDES', error.message);
+        });
+}
+
+function writeToFile() {
+    const writableStream = fs.createWriteStream(FILE_NAME_RESULT);
+    const RESULT_FILE_COLUMNS = ['timestamp', 'precipitation', 'temperature', 'wind', 'rides', 'total_rides'];
+    const stringifier = stringify({ header: true, columns: RESULT_FILE_COLUMNS });
+
+    weatherRows.forEach((row) => stringifier.write(row));
+    stringifier.pipe(writableStream);
+
+    console.log(`Wrote ${weatherRows.length} lines to ${FILE_NAME_RESULT}!`);
+}
+
+importWeatherRows();
